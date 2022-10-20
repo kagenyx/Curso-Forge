2f5fa8917718eb0201afa4378f5fb16da035d3a1
Gonçalo Silva <goncalo.ribeiro.silva@live.com.pt>
2022-10-20 20:39:47 +0100

    adicionei e mudei

---
+++ src/main/java/com/kagenyx/cursomods/item/ItemsMod.java
@@ -0,0 +1,11 @@
+package com.kagenyx.cursomods.item;
+
+import com.kagenyx.cursomods.CursoMods;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemsMod {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CursoMods.EXAMPLEMOD);
+}
