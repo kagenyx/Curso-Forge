31a368e9d07508b9a5f20e74d8a7a6430bd0d8b6
Gonçalo Silva <goncalo.ribeiro.silva@live.com.pt>
2022-11-07 20:09:57 +0000

    added loot tables for raw cobalt block & deepslate cobalt ore

--- src/main/java/com/kagenyx/cursomods/item/ItemsMod.java
+++ src/main/java/com/kagenyx/cursomods/item/ItemsMod.java
@@ -15,6 +15,9 @@ public class ItemsMod {
     public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
 
+    public static final RegistryObject<Item> COBALT_NUGGET = ITEMS.register("cobalt_nugget",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
     public static final RegistryObject<Item> RAW_COBALT = ITEMS.register("raw_cobalt",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
 
